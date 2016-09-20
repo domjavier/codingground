20e2fd54b4e22f6451ec08bb7f6ef3df2f036e38
domjavier <gtrithon@gmail.com>
2016-09-20 16:24:44 +0100

    Created by Coding Ground Client

---
+++ New Project-20160920/main.py
@@ -0,0 +1,6 @@
+#Allows the user to insert a string 
+string=input("Insert the words you wish to be reversed: ")
+#This reverses the chosen words and assigns them to a variable
+reverse_string= string [::-1]
+#This prints the reversed words
+print ("These are your words in reverse: ",reverse_string)
